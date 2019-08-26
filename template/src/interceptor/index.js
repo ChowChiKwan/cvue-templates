eafe76f142caf7b849ca3f71a5dda0ae2dd35dbf
ex_zouzj1 <ex_zouzj1@midea.com>
2019-08-26 17:35:51 +0800

    update

--- template/src/interceptor/index.js
+++ template/src/interceptor/index.js
@@ -2,7 +2,7 @@
  * Created by ChowChiKwan on 2019/08/26.
  */
 import axios from 'axios';
-import { message } from '@/utils';
+import { Message } from 'element-ui';
 
 axios.defaults.baseURL = '/';
 
@@ -15,18 +15,20 @@ axios.interceptors.response.use(
   (res) => {
     if (res && res.data && res.data.code) {
       if (res.data.code !== '0') {
-        message({
+        Message({
           type: 'error',
           message: res.data.msg,
+          duration: '1500',
         });
       }
     }
     return res;
   },
   (err) => {
-    message({
+    Message({
       type: 'error',
       message: err,
+      duration: '1500',
     });
     return Promise.reject(err);
   },
